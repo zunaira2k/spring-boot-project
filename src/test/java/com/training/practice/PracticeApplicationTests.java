1b69f9014aeb7b5589bf88fbeb1dea7c185020cf
Xunaicious <zunaira2k@gmail.com>
2022-06-30 15:20:36 +0530

    spring boot project

---
+++ src/test/java/com/training/practice/PracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.training.practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
